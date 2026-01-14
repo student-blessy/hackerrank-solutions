b7445f3e2776c2da0eb0544caed0b117841f2218
student-blessy <blessyjose488@gmail.com>
2026-01-14 20:03:52 +0530

    Add multiplication table program in test1.java

---
+++ test1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=10;
+        
+        for(int i=1;i<=n;i++){
+            System.out.println(m+" "+"x"+" "+i+" "+"="+" "+m*i);
+
+        }
+  }
+}
